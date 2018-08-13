11695b02696c729ab776db3c37bd4874e894dee1
LasseNatvig <Lasse.Natvig@ntnu.no>
2018-08-13 16:41:30 +0200

    Fixed directory for sasm files

--- _Console/_src/main.cpp
+++ _Console/_src/main.cpp
@@ -7,8 +7,10 @@ int main(int argc, char *argv[]) {
 	std::string sasmPath; // Path to catalog for sasm files
 	if (argc >= 2) // argv[0] is program name
 		sasmPath = argv[1];
-	else
-		sasmPath = "./"; // Read from current directory
+	else {
+		// sasmPath = "./"; // Read from current directory
+		sasmPath = "../../_Simulator/_sasm/"; // TODO-LN Is where I found sasmfiles
+	}
 	ConsoleUi ui(sasmPath);
 	ui.start();
 	return 0;
