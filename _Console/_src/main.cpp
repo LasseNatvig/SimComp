5b2c9e9749f62f138623896990281158522c13e3
LasseNatvig <Lasse.Natvig@ntnu.no>
2018-08-13 19:41:53 +0200

    some clean up in main, if and else-branch should be full { } block

--- _Console/_src/main.cpp
+++ _Console/_src/main.cpp
@@ -5,11 +5,11 @@
 
 int main(int argc, char *argv[]) {
 	std::string sasmPath; // Path to catalog for sasm files
-	if (argc >= 2) // argv[0] is program name
+	if (argc >= 2) { // argv[0] is program name
 		sasmPath = argv[1];
+	}
 	else {
-		// sasmPath = "./"; // Read from current directory
-		sasmPath = "../../_Simulator/_sasm/"; // TODO-LN Is where I found sasmfiles
+		sasmPath = "../../_Simulator/_sasm/"; 
 	}
 	ConsoleUi ui(sasmPath);
 	ui.start();
